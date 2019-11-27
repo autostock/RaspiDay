41ce9b343e9832b4e078db46ba5213c4be4f8e15
autostock <haikuvm@gmail.com>
2019-11-27 12:48:59 +0100

    update README

--- day4/use1.py
+++ day4/use1.py
@@ -3,16 +3,16 @@
 
 from keras.models import load_model
 
-model=load_model('model_day4_ep2.hdf5')
+model=load_model('./myWeights.hdf5')
 
-im = imageio.imread('vier28n.png')
+im = imageio.imread('./dig5.png')
 pixim = np.empty([1,28,28,1])   
 for i in range(0,28):
     for j in range(0,28):
         pixim[0][i][j][0] = im[i][j]/255
         
 N=1        
-font_labels = [4]
+font_labels = [5]
 probabilities = model.predict(pixim, steps=1)
 predicted_labels = np.argmax(probabilities, axis=1)
 print(predicted_labels)
