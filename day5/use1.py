7461d7f5db840eea4c0766eb48915ab411340fe5
autostock <haikuvm@gmail.com>
2019-12-14 22:41:34 +0100

    day5 on Raspi

---
+++ day5/use1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from keras.models import load_model
+import numpy as np, imageio 
+
+#model=load_model('./myWeights.hdf5')
+model=load_model('./model_day4_ep2.hdf5')
+
+im = imageio.imread('./dig5.png')
+#im = imageio.imread('./vier28n.png')
+pixim = np.empty([1,28,28,1])   
+for i in range(0,28):
+    for j in range(0,28):
+        pixim[0][i][j][0] = im[i][j]/255
+        
+N=1        
+font_labels = [5]
+probabilities = model.predict(pixim, steps=1)
+predicted_labels = np.argmax(probabilities, axis=1)
+print(predicted_labels)
+print(probabilities)
